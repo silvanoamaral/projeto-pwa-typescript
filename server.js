b76a8310ee40e48c42dac941a732a294da168002
Silvano Gomes Do Amaral <silvano.amaral9@gmail.com>
2020-11-26 21:43:51 -0300

    add requireHTTPS

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (process.env.NODE_ENV === "production") {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      return next();
+    }
+  }
+  return next();
+}
+
+app.use(requireHTTPS);
+
+app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/ping", function (req, res) {
+  return res.send("pong");
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 app.listen(port);
