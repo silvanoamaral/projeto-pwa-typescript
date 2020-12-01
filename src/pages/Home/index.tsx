2f5dfaa3b7587da9453ae1c0dbee19b9db4d1862
Silvano Gomes Do Amaral <silvano.amaral9@gmail.com>
2020-12-01 16:55:42 -0300

    aplicando funcionalidade para abrir camera no mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import logo from "../../logo.svg";
+import AcesssCamera from "../../components/AccessCamera";
 
 function Home() {
   return (
@@ -22,6 +23,7 @@ function Home() {
       </a>
       <p>PWA (Progressive Web Apps)</p>
       <p>{process.env.REACT_APP_NOT_SECRET_CODE}</p>
+      <AcesssCamera />
     </>
   );
 }
